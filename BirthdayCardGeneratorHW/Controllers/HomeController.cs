881943541a8222176dce9d855178542ca70a519a
edugc310 <eduardo.garcia5@upr.edu>
2022-05-01 20:13:02 -0700

    Add files via upload

---
+++ BirthdayCardGeneratorHW/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using BirthdayCardGenerator.Models;
+using BirthdayCardGeneratorHW.Models;
+
+namespace BirthdayCardGenerator.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpGet]
+    public IActionResult BirthdayCard()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult BirthdayCard(Models.FromTo fromTo)
+    {
+        if (ModelState.IsValid)
+        {
+            return View("Thanks", fromTo);
+        }
+        else
+        {
+            return View();
+        }
+    }
+}
+
---
+++ BirthdayCardGeneratorHW/Models/FromTo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace BirthdayCardGenerator.Models
+{
+	public class FromTo
+	{
+        [Required(ErrorMessage = "Please enter From")]
+        public string From { get; set; }
+        [Required(ErrorMessage = "Please enter To")]
+        public string To { get; set; }
+        [Required(ErrorMessage = "Please enter Message")]
+        public string Message { get; set; }
+        //public string WillAttend { get; set; }
+    }
+}
+
